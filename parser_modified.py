9ebf3f6dcef2948a2587f54b1632fbe7a9d85d89
prabhatp <prabhatp251@gmail.com>
2013-03-09 23:27:40 +0530

    Update parser_modified.py

--- parser_modified.py
+++ parser_modified.py
@@ -18,37 +18,21 @@ precedence = (
 def p_goal_symbol(p):
     '''goal_symbol : compilation'''
     pass
-def p_pragma(p):
-    '''pragma  : PRAGMA IDENTIFIER SEMICOLON
-    | PRAGMA simple_name LPAREN pragma_arg_s RPAREN SEMICOLON'''
-    pass
-def p_pragma_arg_s(p):
-    '''pragma_arg_s : pragma_arg
-    | pragma_arg_s COMMA pragma_arg'''
-    pass
-def p_pragma_arg(p):
-    '''pragma_arg : expression
-    | simple_name ARROW expression'''
-    pass
-def p_pragma_s(p):
-    '''pragma_s :
-    | pragma_s pragma'''
-    pass
+
+#pragmas
+
+#modified
 def p_decl(p):
     '''decl    : object_decl
     | number_decl
     | type_decl
     | subtype_decl
     | subprog_decl
-    | pkg_decl
-    | task_decl
-    | prot_decl
-    | exception_decl
-    | rename_decl
     | generic_decl
-    | body_stub
     | error SEMICOLON'''
     pass
+#end
+
 def p_object_decl(p):
     '''object_decl : def_id_s COLON object_qualifier_opt object_subtype_def init_opt SEMICOLON'''
     pass
@@ -88,16 +72,18 @@ def p_type_completion(p):
     '''type_completion :
     | IS type_def'''
     pass
+
+#modified
 def p_type_def(p):
     '''type_def : enumeration_type 
     | integer_type
     | real_type
     | array_type
     | record_type
-    | access_type
-    | derived_type
-    | private_type'''
+    | access_type'''
     pass
+#end
+
 def p_subtype_decl(p):
     '''subtype_decl : SUBTYPE IDENTIFIER IS subtype_ind SEMICOLON'''
     pass
@@ -112,13 +98,9 @@ def p_constraint(p):
 def p_decimal_digits_constraint(p):
     '''decimal_digits_constraint : DIGITS expression range_constr_opt'''
     pass
-def p_derived_type(p):
-    '''derived_type : NEW subtype_ind
-    | NEW subtype_ind WITH PRIVATE
-    | NEW subtype_ind WITH record_def
-    | ABSTRACT NEW subtype_ind WITH PRIVATE
-    | ABSTRACT NEW subtype_ind WITH record_def'''
-    pass
+
+#derived_type
+
 def p_range_constraint(p):
     '''range_constraint : RANGE range'''
     pass
@@ -128,9 +110,10 @@ def p_range(p):
     | name TICK RANGE LPAREN expression RPAREN'''
     pass
 def p_enumeration_type(p):
-    '''enumeration_type : LPAREN enum_id_s RPAREN
-
-enum_id_s : enum_id
+    '''enumeration_type : LPAREN enum_id_s RPAREN'''
+    pass
+def p_enum_id_s(p):
+    '''enum_id_s : enum_id
     | enum_id_s COMMA enum_id'''
     pass
 def p_enum_id(p):
@@ -210,6 +193,8 @@ def p_tagged_opt(p):
     | TAGGED
     | ABSTRACT TAGGED'''
     pass
+
+#modified
 def p_comp_list(p):
     '''comp_list : comp_decl_s variant_part_opt
     | variant_part
@@ -223,6 +208,8 @@ def p_variant_part_opt(p):
     '''variant_part_opt : 
     | variant_part'''
     pass
+#end
+
 def p_comp_decl(p):
     '''comp_decl : def_id_s COLON component_subtype_def init_opt SEMICOLON
     | error SEMICOLON'''
@@ -242,16 +229,20 @@ def p_access_opt(p):
     '''access_opt :
     | ACCESS'''
     pass
+
+#modified
 def p_variant_part(p):
-    '''variant_part : CASE simple_name IS pragma_s variant_s END CASE SEMICOLON'''
+    '''variant_part : CASE simple_name IS variant_s END CASE SEMICOLON'''
     pass
 def p_variant_s(p):
     '''variant_s : variant
     | variant_s variant'''
     pass
 def p_variant(p):
-    '''variant : WHEN choice_s ARROW pragma_s comp_list'''
+    '''variant : WHEN choice_s ARROW comp_list'''
     pass
+#end
+
 def p_choice_s(p):
     '''choice_s : choice
     | choice_s '|' choice'''
@@ -280,20 +271,16 @@ def p_decl_part(p):
     '''decl_part :
     | decl_item_or_body_s1'''
     pass
-def p_decl_item_s(p):
-    '''decl_item_s : 
-    | decl_item_s1'''
-    pass
-def p_decl_item_s1(p):
-    '''decl_item_s1 : decl_item
-    | decl_item_s1 decl_item'''
-    pass
+
+#decl_item --> unused
+
+#modified
 def p_decl_item(p):
     '''decl_item : decl
-    | use_clause
-    | rep_spec
-    | pragma'''
+    | use_clause'''
     pass
+#end
+
 def p_decl_item_or_body_s1(p):
     '''decl_item_or_body_s1 : decl_item_or_body
     | decl_item_or_body_s1 decl_item_or_body'''
@@ -302,12 +289,13 @@ def p_decl_item_or_body(p):
     '''decl_item_or_body : body
     | decl_item'''
     pass
+
+#modified
 def p_body(p):
-    '''body : subprog_body
-    | pkg_body
-    | task_body
-    | prot_body'''
+    '''body : subprog_body'''
     pass
+#end
+
 def p_name(p):
     '''name : simple_name
     | indexed_comp
@@ -468,14 +456,14 @@ def p_statement_s(p):
     '''statement_s : statement
     | statement_s statement'''
     pass
+
+#modified
 def p_statement(p):
-    '''statement : unlabeled
-    | label statement'''
+    '''statement : unlabeled'''
     pass
 def p_unlabeled(p):
     '''unlabeled : simple_stmt
-    | compound_stmt
-    | pragma'''
+    | compound_stmt'''
     pass
 def p_simple_stmt(p):
     '''simple_stmt : NULL_stmt
@@ -484,24 +472,18 @@ def p_simple_stmt(p):
     | return_stmt
     | goto_stmt
     | procedure_call
-    | delay_stmt
-    | abort_stmt
-    | raise_stmt
-    | code_stmt
-    | requeue_stmt
     | error SEMICOLON'''
     pass
 def p_compound_stmt(p):
     '''compound_stmt : if_stmt
     | case_stmt
     | loop_stmt
-    | block
-    | accept_stmt
-    | select_stmt'''
+    | block'''
     pass
-def p_label(p):
-    '''label : LLB IDENTIFIER RLB'''
-    pass
+#end
+
+#label
+
 def p_NULL_stmt(p):
     '''NULL_stmt : NULL SEMICOLON'''
     pass
@@ -528,9 +510,13 @@ def p_else_opt(p):
     '''else_opt :
     | ELSE statement_s'''
     pass
+
+#modified
 def p_case_stmt(p):
     '''case_stmt : CASE expression IS alternative_s END CASE SEMICOLON'''
     pass
+#end
+
 def p_alternative_s(p):
     '''alternative_s :
     | alternative_s alternative'''
@@ -564,23 +550,27 @@ def p_id_opt(p):
     '''id_opt :
     | designator'''
     pass
+
+#modified
 def p_block(p):
-    '''block : label_opt block_decl block_body END id_opt SEMICOLON'''
+    '''block : label_opt block_body END id_opt SEMICOLON'''
     pass
-def p_block_decl(p):
-    '''block_decl :
-    | DECLARE decl_part'''
-    pass
+#end
+
+#block_decl
+
 def p_block_body(p):
     '''block_body : BEGIN handled_stmt_s'''
     pass
+
+#modified
 def p_handled_stmt_s(p):
-    '''handled_stmt_s : statement_s except_handler_part_opt'''
+    '''handled_stmt_s : statement_s'''
     pass
-def p_except_handler_part_opt(p):
-    '''except_handler_part_opt :
-    | except_handler_part'''
-    pass
+#end
+
+#exception_handle
+
 def p_exit_stmt(p):
     '''exit_stmt : EXIT name_opt when_opt SEMICOLON'''
     pass
@@ -599,11 +589,14 @@ def p_return_stmt(p):
 def p_goto_stmt(p):
     '''goto_stmt : GOTO name SEMICOLON'''
     pass
+
+#modified
 def p_subprog_decl(p):
     '''subprog_decl : subprog_spec SEMICOLON
-    | generic_subp_inst SEMICOLON
-    | subprog_spec_is_push ABSTRACT SEMICOLON'''
+    | generic_subp_inst SEMICOLON'''
     pass
+#end
+
 def p_subprog_spec(p):
     '''subprog_spec : PROCEDURE compound_name formal_part_opt
     | FUNCTION designator formal_part_opt RETURN name
@@ -644,31 +637,10 @@ def p_subprog_body(p):
 def p_procedure_call(p):
     '''procedure_call : name SEMICOLON'''
     pass
-def p_pkg_decl(p):
-    '''pkg_decl : pkg_spec SEMICOLON
-    | generic_pkg_inst SEMICOLON'''
-    pass
-def p_pkg_spec(p):
-    '''pkg_spec : PACKAGE compound_name IS decl_item_s private_part END c_id_opt'''
-    pass
-def p_private_part(p):
-    '''private_part :
-    | PRIVATE decl_item_s'''
-    pass
-def p_c_id_opt(p):
-    '''c_id_opt : 
-    | compound_name'''
-    pass
-def p_pkg_body(p):
-    '''pkg_body : PACKAGE BODY compound_name IS decl_part body_opt END c_id_opt SEMICOLON'''
-    pass
-def p_body_opt(p):
-    '''body_opt :
-    | block_body'''
-    pass
-def p_private_type(p):
-    '''private_type : tagged_opt limited_opt PRIVATE'''
-    pass
+
+#pkg_decl
+#private_type
+
 def p_limited_opt(p):
     '''limited_opt :
     | LIMITED'''
@@ -681,174 +653,24 @@ def p_name_s(p):
     '''name_s : name
     | name_s COMMA name'''
     pass
-def p_rename_decl(p):
-    '''rename_decl : def_id_s COLON object_qualifier_opt subtype_ind renames SEMICOLON
-    | def_id_s COLON EXCEPTION renames SEMICOLON
-    | rename_unit'''
-    pass
-def p_rename_unit(p):
-    '''rename_unit : PACKAGE compound_name renames SEMICOLON
-    | subprog_spec renames SEMICOLON
-    | generic_formal_part PACKAGE compound_name renames SEMICOLON
-    | generic_formal_part subprog_spec renames SEMICOLON'''
-    pass
-def p_renames(p):
-    '''renames : RENAMES name'''
-    pass
-def p_task_decl(p):
-    '''task_decl : task_spec SEMICOLON'''
-    pass
-def p_task_spec(p):
-    '''task_spec : TASK simple_name task_def
-    | TASK TYPE simple_name discrim_part_opt task_def'''
-    pass
-def p_task_def(p):
-    '''task_def :
-    | IS entry_decl_s rep_spec_s task_private_opt END id_opt'''
-    pass
-def p_task_private_opt(p):
-    '''task_private_opt :
-    | PRIVATE entry_decl_s rep_spec_s'''
-    pass
-def p_task_body(p):
-    '''task_body : TASK BODY simple_name IS decl_part block_body END id_opt SEMICOLON'''
-    pass
-def p_prot_decl(p):
-    '''prot_decl : prot_spec SEMICOLON'''
-    pass
-def p_prot_spec(p):
-    '''prot_spec : PROTECTED IDENTIFIER prot_def
-    | PROTECTED TYPE simple_name discrim_part_opt prot_def'''
-    pass
-def p_prot_def(p):
-    '''prot_def : IS prot_op_decl_s prot_private_opt END id_opt'''
-    pass
-def p_prot_private_opt(p):
-    '''prot_private_opt :
-    | PRIVATE prot_elem_decl_s '''
-    pass
-def p_prot_op_decl_s(p):
-    '''prot_op_decl_s :
-    | prot_op_decl_s prot_op_decl'''
-    pass
-def p_prot_op_decl(p):
-    '''prot_op_decl : entry_decl
-    | subprog_spec SEMICOLON
-    | rep_spec
-    | pragma'''
-    pass
-def p_prot_elem_decl_s(p):
-    '''prot_elem_decl_s : 
-    | prot_elem_decl_s prot_elem_decl'''
-    pass
-def p_prot_elem_decl(p):
-    '''prot_elem_decl : prot_op_decl 
-    | comp_decl'''
-    pass
-def p_prot_body(p):
-    '''prot_body : PROTECTED BODY simple_name IS prot_op_body_s END id_opt SEMICOLON'''
-    pass
-def p_prot_op_body_s(p):
-    '''prot_op_body_s : pragma_s
-    | prot_op_body_s prot_op_body pragma_s'''
-    pass
-def p_prot_op_body(p):
-    '''prot_op_body : entry_body
-    | subprog_body
-    | subprog_spec SEMICOLON'''
-    pass
-def p_entry_decl_s(p):
-    '''entry_decl_s : pragma_s
-    | entry_decl_s entry_decl pragma_s'''
-    pass
-def p_entry_decl(p):
-    '''entry_decl : ENTRY IDENTIFIER formal_part_opt SEMICOLON
-    | ENTRY IDENTIFIER LPAREN discrete_range RPAREN formal_part_opt SEMICOLON'''
-    pass
-def p_entry_body(p):
-    '''entry_body : ENTRY IDENTIFIER formal_part_opt WHEN condition entry_body_part
-    | ENTRY IDENTIFIER LPAREN iter_part discrete_range RPAREN formal_part_opt WHEN condition entry_body_part'''
-    pass
-def p_entry_body_part(p):
-    '''entry_body_part : SEMICOLON
-    | IS decl_part block_body END id_opt SEMICOLON'''
-    pass
-def p_rep_spec_s(p):
-    '''rep_spec_s :
-    | rep_spec_s rep_spec pragma_s'''
-    pass
-def p_entry_call(p):
-    '''entry_call : procedure_call'''
-    pass
-def p_accept_stmt(p):
-    '''accept_stmt : accept_hdr SEMICOLON
-    | accept_hdr DO handled_stmt_s END id_opt SEMICOLON'''
-    pass
-def p_accept_hdr(p):
-    '''accept_hdr : ACCEPT entry_name formal_part_opt'''
-    pass
-def p_entry_name(p):
-    '''entry_name : simple_name
-    | entry_name LPAREN expression RPAREN'''
-    pass
-def p_delay_stmt(p):
-    '''delay_stmt : DELAY expression SEMICOLON
-    | DELAY UNTIL expression SEMICOLON'''
-    pass
-def p_select_stmt(p):
-    '''select_stmt : select_wait
-    | async_select
-    | timed_entry_call
-    | cond_entry_call'''
-    pass
-def p_select_wait(p):
-    '''select_wait : SELECT guarded_select_alt or_select else_opt END SELECT SEMICOLON'''
-    pass
-def p_guarded_select_alt(p):
-    '''guarded_select_alt : select_alt
-    | WHEN condition ARROW select_alt'''
-    pass
-def p_or_select(p):
-    '''or_select :
-    | or_select OR guarded_select_alt'''
-    pass
-def p_select_alt(p):
-    '''select_alt : accept_stmt stmts_opt
-    | delay_stmt stmts_opt
-    | TERMINATE SEMICOLON'''
-    pass
 
-def p_delay_or_entry_alt(p):
-    '''delay_or_entry_alt : delay_stmt stmts_opt
-    | entry_call stmts_opt'''
-    pass
+#renames
+#task
+#prot
+#entry
+#accept_stmt
+#delay_stmt
+#select_stmt
+#abort_stmt
 
-def p_async_select(p):
-    '''async_select : SELECT delay_or_entry_alt THEN ABORT statement_s END SELECT SEMICOLON'''
-    pass
-
-def p_timed_entry_call(p):
-    '''timed_entry_call : SELECT entry_call stmts_opt OR delay_stmt stmts_opt END SELECT SEMICOLON'''
-    pass
-def p_cond_entry_call(p):
-    '''cond_entry_call : SELECT entry_call stmts_opt ELSE statement_s END SELECT SEMICOLON'''
-    pass
-
-def p_stmts_opt(p):
-    '''stmts_opt :
-    | statement_s'''
-    pass
-def p_abort_stmt(p):
-    '''abort_stmt : ABORT name_s SEMICOLON'''
-    pass
+#modified
 def p_compilation(p):
     '''compilation :
-    | compilation comp_unit
-    | pragma pragma_s'''
+    | compilation comp_unit'''
     pass
 def p_comp_unit(p):
-    '''comp_unit : context_spec private_opt unit pragma_s
-    | private_opt unit pragma_s'''
+    '''comp_unit : context_spec private_opt unit
+    | private_opt unit'''
     pass
 def p_private_opt(p):
     '''private_opt :
@@ -856,9 +678,10 @@ def p_private_opt(p):
     pass
 def p_context_spec(p):
     '''context_spec : with_clause use_clause_opt
-    | context_spec with_clause use_clause_opt
-    | context_spec pragma'''
+    | context_spec with_clause use_clause_opt'''
     pass
+#end
+
 def p_with_clause(p):
     '''with_clause : WITH c_name_list SEMICOLON'''
     pass
@@ -866,134 +689,49 @@ def p_use_clause_opt(p):
     '''use_clause_opt :
     | use_clause_opt use_clause'''
     pass
+
+#modified
 def p_unit(p):
-    '''unit : pkg_decl
-    | pkg_body
-    | subprog_decl
+    '''unit : subprog_decl
     | subprog_body
-    | subunit
-    | generic_decl
-    | rename_unit'''
+    | generic_decl'''
     pass
-def p_subunit(p):
-    '''subunit : SEPARATE LPAREN compound_name RPAREN subunit_body'''
-    pass
-def p_subunit_body(p):
-    '''subunit_body : subprog_body
-    | pkg_body
-    | task_body
-    | prot_body'''
-    pass
-def p_body_stub(p):
-    '''body_stub : TASK BODY simple_name IS SEPARATE SEMICOLON
-    | PACKAGE BODY compound_name IS SEPARATE SEMICOLON
-    | subprog_spec IS SEPARATE SEMICOLON
-    | PROTECTED BODY simple_name IS SEPARATE SEMICOLON'''
-    pass
-def p_exception_decl(p):
-    '''exception_decl : def_id_s COLON EXCEPTION SEMICOLON'''
-    pass
-def p_except_handler_part(p):
-    '''except_handler_part : EXCEPTION exception_handler
-    | except_handler_part exception_handler'''
-    pass
-def p_exception_handler(p):
-    '''exception_handler : WHEN except_choice_s ARROW statement_s
-    | WHEN IDENTIFIER COLON except_choice_s ARROW statement_s'''
-    pass
-def p_except_choice_s(p):
-    '''except_choice_s : except_choice
-    | except_choice_s '|' except_choice'''
-    pass
-def p_except_choice(p):
-    '''except_choice : name
-    | OTHERS'''
-    pass
-def p_raise_stmt(p):
-    '''raise_stmt : RAISE name_opt SEMICOLON'''
-    pass
-def p_requeue_stmt(p):
-    '''requeue_stmt : REQUEUE name SEMICOLON
-    | REQUEUE name WITH ABORT SEMICOLON'''
-    pass
+#end
+
+#body_stub
+#exception
+#raise_stmt
+#reque_stmt
+
+#modified
 def p_generic_decl(p):
-    '''generic_decl : generic_formal_part subprog_spec SEMICOLON
-    | generic_formal_part pkg_spec SEMICOLON'''
+    '''generic_decl : generic_formal_part subprog_spec SEMICOLON'''
     pass
+#end
+
 def p_generic_formal_part(p):
     '''generic_formal_part : GENERIC
     | generic_formal_part generic_formal'''
     pass
 def p_generic_formal(p):
     '''generic_formal : param SEMICOLON
-    | TYPE simple_name generic_discrim_part_opt IS generic_type_def SEMICOLON
-    | WITH PROCEDURE simple_name formal_part_opt subp_default SEMICOLON
-    | WITH FUNCTION designator formal_part_opt RETURN name subp_default SEMICOLON
-    | WITH PACKAGE simple_name IS NEW name LPAREN BOX RPAREN SEMICOLON
-    | WITH PACKAGE simple_name IS NEW name SEMICOLON
     | use_clause'''
     pass
-def p_generic_discrim_part_opt(p):
-    '''generic_discrim_part_opt :
-    | discrim_part
-    | LPAREN BOX RPAREN'''
-    pass
-def p_subp_default(p):
-    '''subp_default :
-    | IS name
-    | IS BOX'''
-    pass
-def p_generic_type_def(p):
-    '''generic_type_def : LPAREN BOX RPAREN
-    | RANGE BOX
-    | MOD BOX
-    | DELTA BOX
-    | DELTA BOX DIGITS BOX
-    | DIGITS BOX
-    | array_type
-    | access_type
-    | private_type
-    | generic_derived_type'''
-    pass
-def p_generic_derived_type(p):
-    '''generic_derived_type : NEW subtype_ind
-    | NEW subtype_ind WITH PRIVATE
-    | ABSTRACT NEW subtype_ind WITH PRIVATE'''
-    pass
+
+#generic_related
+
 def p_generic_subp_inst(p):
     '''generic_subp_inst : subprog_spec IS generic_inst'''
     pass
-def p_generic_pkg_inst(p):
-    '''generic_pkg_inst : PACKAGE compound_name IS generic_inst'''
-    pass
+
+#generic_pkg
+
 def p_generic_inst(p):
     '''generic_inst : NEW name'''
     pass
-def p_rep_spec(p):
-    '''rep_spec : attrib_def
-    | record_type_spec
-    | address_spec'''
-    pass
-def p_attrib_def(p):
-    '''attrib_def : FOR mark USE expression SEMICOLON'''
-    pass
-def p_record_type_spec(p):
-    '''record_type_spec : FOR mark USE RECORD align_opt comp_loc_s END RECORD SEMICOLON'''
-    pass
-def p_align_opt(p):
-    '''align_opt :
-    | AT MOD expression SEMICOLON'''
-    pass
-def p_comp_loc_s(p):
-    '''comp_loc_s :
-    | comp_loc_s mark AT expression RANGE range SEMICOLON'''
-    pass
-def p_address_spec(p):
-    '''address_spec : FOR mark USE AT expression SEMICOLON'''
-    pass
-def p_code_stmt(p):
-    '''code_stmt : qualified SEMICOLON'''
-    pass
+
+#rep_spec
+#code_stmt
 
 def p_empty(t):
     'empty : '
@@ -1008,23 +746,21 @@ def p_error(p):
 
 yacc = yacc.yacc()
 cu = '''
-with Ada.Text_IO; use Ada.Text_IO;
-
-procedure Hello is
-type Day_type is range 1 .. 31;
-type Month_type is range 1 .. 12;
-type Year_type is range 1800 .. 2100;
-type Hours is mod 24;
-type Weekday is (Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday);
+function Levenshtein(Left, Right : String) return Natural is
+    D : array(0 .. Left'Last, 0 .. Right'Last) of Natural;
+  begin
+    for I in D'range(1) loop D(I, 0) := I;end loop;
  
-type Date is
-   record
-     Day : Day_type;
-     Month : Month_type;
-     Year : Year_type;
-   end record;
-begin
-  Put_Line ("Hello, world!");
-end Hello;
+    for J in D'range(2) loop D(0, J) := J;end loop;
+ 
+    for I in Left'range loop
+      for J in Right'range loop
+        D(I, J) := Natural'Min(D(I - 1, J), D(I, J - 1)) + 1;
+        D(I, J) := Natural'Min(D(I, J), D(I - 1, J - 1) + Boolean'Pos(Left(I) /= Right(J)));
+      end loop;
+    end loop;
+ 
+    return D(D'Last(1), D'Last(2));
+  end Levenshtein;
 '''
 print yacc.parse(cu)
