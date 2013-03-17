429f7003942aa752856e112374fe6e1e41d7927a
euclid-curry <gargmanav92@gmail.com>
2013-03-18 00:49:42 +0530

    Update loo.py

--- loo.py
+++ loo.py
@@ -1,40 +1,6 @@
-# exprtype.py
-'''
-Expr Type System
-================
-This file defines classes representing types.  There is a general
-class used to represent all types.  Each type is then a singleton
-instance of the type class.
-
-class ExprType(object):
-      pass
-
-int_type = ExprType("int",...)
-float_type = ExprType("float",...)
-string_type = ExprType("string", ...)
-
-The contents of the type class is entirely up to you.  However, you
-will minimally need to encode some information about:
-
-   a.  What operators are supported (+, -, *, etc.).
-   b.  Default values
-   c.  ????
-   d.  Profit!
-
-Once you have defined the built-in types, you will need to
-make sure they get registered with any symbol tables or
-code that checks for type names in 'exprcheck.py'.
-
-Note:  This file is expanded in later stages of the compiler project.
-'''
-
 import operator
 
 class ExprType(object):
-    '''
-    Class that represents a type in the Expr language.  Types 
-    are declared as singleton instances of this type.
-    '''
     def __init__(self, typename, default, 
                  unary_opcodes=None, binary_opcodes=None, 
                  binary_ops=None, unary_ops=None,
@@ -100,6 +66,42 @@ BoolType = ExprType("bool", bool(),
 )
 
 CharType = ExprType("character",str(),
+    unary_ops={},
+    rel_ops={},
+    rel_opcodes={},
+    rel_folds={},
+    unary_opcodes={},
+    unary_folds={}
+)
+
+EnumType = ExprType("enumeration",None,
+    unary_ops={},
+    rel_ops={},
+    rel_opcodes={},
+    rel_folds={},
+    unary_opcodes={},
+    unary_folds={}
+)
+
+ArrayType = ExprType("array",None,
+    unary_ops={},
+    rel_ops={},
+    rel_opcodes={},
+    rel_folds={},
+    unary_opcodes={},
+    unary_folds={}
+)
+
+AccessType = ExprType("access",None,
+    unary_ops={},
+    rel_ops={},
+    rel_opcodes={},
+    rel_folds={},
+    unary_opcodes={},
+    unary_folds={}
+)
+
+RecordType = ExprType("record",None,
     unary_ops={},
     rel_ops={},
     rel_opcodes={},
