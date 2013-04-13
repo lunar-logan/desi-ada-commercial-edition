d8ac3321be5a7fbad871a8cb5b63abde5baf67c1
Euclid <euclid@ubuntu.ubuntu-domain>
2013-04-13 21:37:01 +0530

    Remove duplicated directory

--- cs335/cs335/square.adb
+++
@@ -1,9 +0,0 @@
--- Change return type. Provides return type error
-function Square (Arg: Integer) return Integer is
---   B : float;
-   begin
-        Arg := 3;
---      B := 4.5;
---      return B;
-      return Arg * Arg;
-   end Square;
--- cs335/test2.adb
+++
@@ -1,8 +0,0 @@
--- Change return type. Provides return type error
-function Square (Arg : Integer) return Integer is
---   B : float;
-   begin
---      B := 4.5;
---      return B;
-      return Arg * Arg;
-   end Square;
